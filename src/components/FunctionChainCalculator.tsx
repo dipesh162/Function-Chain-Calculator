9a5a4cf1626631506a72ab6fb711fc2612bff0c2
dipesh162 <dipeshsingh162@gmail.com>
2024-10-03 20:59:42 +0530

    .

--- src/components/FunctionChainCalculator.tsx
+++ src/components/FunctionChainCalculator.tsx
@@ -11,13 +11,13 @@ type Function = {
     nextFunction: number | null
     input: number
     output: number
-    path?: string
+    path?: string | null
 }
 
 const initialFunctions: Function[] = [
     { id: 1, equation: 'x^2', nextFunction: 2, input: 0, output: 0, path: 'quadratic'},
     { id: 2, equation: '2*x+4', nextFunction: 4, input: 0, output: 0, path: 'cubic'},
-    { id: 3, equation: 'x^2+20', nextFunction: null, input: 0, output: 0,},
+    { id: 3, equation: 'x^2+20', nextFunction: null, input: 0, output: 0, path: null},
     { id: 4, equation: 'x-2', nextFunction: 5, input: 0, output: 0, path: 'quadratic' },
     { id: 5, equation: 'x/2', nextFunction: 3, input: 0, output: 0, path: 'bottomTopQuadratic'  },
 ]
@@ -90,7 +90,7 @@ export default function FunctionChainCalculator() {
 
     
         functions.forEach((func) => {
-            if (func.nextFunction !== null) {
+            if (func.path && func.nextFunction) {
                 const outputElem = outputRefs.current[func.id];
                 const nextInputElem = inputRefs.current[func.nextFunction];
     
